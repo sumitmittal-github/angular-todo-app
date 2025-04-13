8e3f8adf8cfc904b11cb5af6cb08a53c03cce869
Sumit Mittal <sumitmittal.adonis@gmail.com>
2025-04-13 16:18:10 -0400

    Added Todo Module and created Routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { HeaderComponent } from './components/header/header.component';
-import { HomeComponent } from './home/home.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, HomeComponent],
+  imports: [HeaderComponent, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        pathMatch: 'full',
+        loadComponent: () => {
+            return import('./home/home.component').then(m => m.HomeComponent)
+        }
+    }, {
+        path: 'home',
+        pathMatch: 'full',
+        loadComponent: () => {
+            return import('./home/home.component').then(m => m.HomeComponent)
+        }
+    }, {
+        path: 'todo',
+        loadComponent: () => {
+            return import('./todo/todo.component').then(m => m.TodoComponent)
+        }
+    }
+
+];
